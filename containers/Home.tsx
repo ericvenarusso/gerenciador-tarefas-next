fad6ec08d15661c20d1f7158409db8ca1fd96985
ericvenarusso2 <eric.venarusso@viavarejo.com.br>
2021-10-23 11:42:50 -0300

    Add token login

---
+++ containers/Home.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+/* eslint-disable @next/next/no-img-element */
+export const Home = () => {
+    return (
+        <div className="container-login">
+            <h1>Home</h1>
+        </div>
+    );
+}
