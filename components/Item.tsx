b5bbf92ed28a19467966940aacd9b5cd048456c5
ericvenarusso2 <eric.venarusso@viavarejo.com.br>
2021-10-27 23:15:29 -0300

    Add modal

--- components/Item.tsx
+++ components/Item.tsx
@@ -5,24 +5,27 @@ import { Task } from "../types/Task";
 
 type ItemProps = {
     task : Task
+    setTaskAndShow(task:Task) : void
 }
 
 const Item: NextPage<ItemProps> = ({
-    task
+    task,
+    setTaskAndShow
 }) => {
 
     const getDateText = (finishDate : any, finishPrevisionDate : any) => {
         if(finishDate){
             const fd = new Date(finishDate)
-            return `Concluido em: ${fd.getDay() + '/' + fd.getMonth() + '/' + fd.getFullYear()}`;
+            return `Concluido em: ${fd.getDate() + '/' + (fd.getMonth()+1) + '/' + fd.getFullYear()}`;
         }
         
         const fpd = new Date(finishPrevisionDate)
-        return `Previsao de conclusao em: ${fpd.getDay() + '/' + fpd.getMonth() + '/' + fpd.getFullYear()}`;
+        return `Previsao de conclusao em: ${fpd.getDate() + '/' + (fpd.getMonth()+1) + '/' + fpd.getFullYear()}`;
     }
 
     return (
-        <div className={"container-item" + (task.finishDate? '' : ' ativo')}>
+        <div className={"container-item" + (task.finishDate? '' : ' ativo')}
+            onClick={() => task.finishDate ? null : setTaskAndShow(task)}>
             <img src={task.finishDate? '/checked.svg' : '/not-checked.svg'} 
                 alt={task.finishDate? 'Tarefa concluida' : 'Tarefa em aberto'}/>
             <div>
